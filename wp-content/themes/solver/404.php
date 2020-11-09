c4a0dd31ef098d4b37448e6679a425ad1ddfbf76
kirill15 <kirill.puzanov@gmail.com>
2020-11-09 03:29:18 +0300

    Test

--- wp-content/themes/solver/404.php
+++ wp-content/themes/solver/404.php
@@ -3,7 +3,7 @@
 <div id="article" class="container" style="margin-top:20px">
   <div class="row">
     <h1 class="article__title">Ошибка!</h1>
-    <center><h3>Воспользуйтесь меню для перехода на нужную страницу сайта</h3></center>
+    <div style="text-align: center;"><h3>Воспользуйтесь меню для перехода на нужную страницу сайта</h3></div>
   </div>
 </div>
 
