baf1628d2ef8059020988fec1a9cd4d1fa2dbe64
kirill15 <kirill.puzanov@gmail.com>
2020-12-06 22:39:57 +0300

    Vector logo and favicon

--- wp-content/themes/solver/header.php
+++ wp-content/themes/solver/header.php
@@ -20,7 +20,7 @@
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     <link rel="manifest" href="/site.webmanifest">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#222222>
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#222222">
     <meta name="msapplication-TileColor" content="#ffc40d">
     <meta name="theme-color" content="#0065cc">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no">
