44cb1f1daeef8b258cbb60dc669466fca682a062
kirill15 <kirill.puzanov@gmail.com>
2021-03-10 21:34:50 +0300

    Fix canonical urls problem in order pages

--- wp-content/plugins/reshatel-order-viewer/ROV.php
+++ wp-content/plugins/reshatel-order-viewer/ROV.php
@@ -161,6 +161,9 @@ class ROV {
         add_filter('aioseo_title', function() use($order) {
             return ($order ? $order->name . ' на заказ': 'Ошибка') . ' | Решатель';
         });
+        add_filter('aioseo_canonical_url', function() use($orderName) {
+            return get_site_url() . "/orders/$orderName/";
+        });
         add_action('wp_head', function() use($order) {
             echo '<meta name="description" content="' . "$order->name - $order->type по $order->cat_dat на заказ" . '" />';
             if (iconv_strlen($order->comments) < 1000) {
